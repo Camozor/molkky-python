8d37ba478892dcf9568710369014bcbc3578b5bc
Camille Vauchel <camille.vauchel@neomi.immo>
2023-06-20 16:11:16 +0200

    Fix test name

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from main import *
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_1_quille(self):
+    def test_addition(self):
         self.assertEqual(addition(9, 16), 25)
 
 if __name__ == '__main__':
