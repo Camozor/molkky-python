602aab69892d2bbf226430079ebefbdf85e6c240
Camille Vauchel <camille.vauchel@neomi.immo>
2023-06-20 16:09:25 +0200

    Some solution

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
-def addition(x, y):
-    return x + y
+def calcule_lancer(lancer):
+    if len(lancer) > 1:
+        return len(lancer)
+    else:
+        return lancer[0]
+
+def calcule_multiple_lancers(lancers):
+    score = 0
+    for lancer in lancers:
+        score += calcule_lancer(lancer)
+        if score > 50:
+            score = 25
+    return score
+
+def est_gagnant(lancers):
+    return calcule_multiple_lancers(lancers) == 50
+
+def est_elimine(lancers):
+    rate_affile = 0
+    for lancer in lancers:
+        if len(lancer) == 0:
+            rate_affile += 1
+        else:
+            rate_affile = 0
+
+        if rate_affile == 3:
+            return True
+    return False 
--- test.py
+++ test.py
@@ -4,7 +4,28 @@ from main import *
 class TestStringMethods(unittest.TestCase):
 
     def test_1_quille(self):
-        self.assertEqual(addition(9, 16), 25)
+        self.assertEqual(calcule_lancer([3]), 3)
+
+    def test_multiple_quilles(self):
+        self.assertEqual(calcule_lancer([3, 5]), 2)
+
+    def test_multiple_lancers(self):
+        self.assertEqual(calcule_multiple_lancers([[6], [2, 5], [4, 7, 9]]), 11)
+
+    def test_est_gagnant(self):
+        self.assertEqual(est_gagnant([[12], [12], [12], [12], [1, 7]]), True)
+
+    def test_multiple_lancers_overflow(self):
+        self.assertEqual(calcule_multiple_lancers([[12], [12], [12], [12], [1, 2, 3]]), 25)
+    
+    def test_multiple_lancers_overflow2(self):
+        self.assertEqual(calcule_multiple_lancers([[12], [12], [12], [12], [1, 2, 3], [3]]), 28)
+
+    def test_non_elimine_plus_de_3(self):
+        self.assertEqual(est_elimine([[3, 10], [], [], [4], [], [6, 8]]), False)
+    
+    def test_est_elimine(self):
+        self.assertEqual(est_elimine([[3, 10], [], [], [], [6, 8]]), True)
 
 if __name__ == '__main__':
     unittest.main()
